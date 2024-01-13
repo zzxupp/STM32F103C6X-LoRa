90a782f1c3bbb4f47cb24cecf1141a4ba20f4898
Zhen Xupin <justdo@email.cn>
2024-01-13 13:17:16 +0800

    Update usart2.c

--- HARDWARE/USART2/usart2.c
+++ HARDWARE/USART2/usart2.c
@@ -1,6 +1,6 @@
 /**
   ***************************************************************************************************
-  * @author  lilin
+  * @author  zzxupp
   * @version V1.0
   * @date    2021.10.16
   * @brief   LoRa模块连接的串口
@@ -23,10 +23,6 @@
   * @retval:无
   */
 
-//接收状态
-//bit15，	接收完成标志
-//bit14，	接收到0x0d
-//bit13~0，	接收到的有效字节数目
 u8 USART2_RX_BUF[USART2_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
 u16 USART2_RX_STA=0;       //接收状态标记	  
 
@@ -65,7 +61,7 @@ void drv_uart_init( uint32_t UartBaudRate )
 	}
 	
 	//Usart1 NVIC 配置
-  NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+        NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=2 ;//抢占优先级3
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;		//子优先级3
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
